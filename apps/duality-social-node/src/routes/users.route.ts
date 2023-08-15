247d2da2b1bb27762f1b68a1758c78518d387031
Jessica Mulein <jessica@digitaldefiance.org>
2023-08-15 21:40:16 +0000

    user registration works

--- apps/duality-social-node/src/routes/users.route.ts
+++ apps/duality-social-node/src/routes/users.route.ts
@@ -5,8 +5,12 @@ export const usersRouter = Router();
 usersRouter.post('/register', async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
   try {
-    const newUser = await UserService.register(email, username, password);
-    res.status(201).json(newUser);
+    await UserService.register(email, username, password);
+    res.status(201).json({
+      message: 'User created successfully',
+      email: email,
+      username: username,
+    });
   }
   catch (error) {
     res.status(400).json(error);
