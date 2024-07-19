7eaf1e74a876f693abce6312d326a737d2337adb
Jessica Mulein <jessica@digitaldefiance.org>
2024-07-19 20:18:54 +0000

    update humanity type in vote schema

--- libs/duality-social-lib/src/lib/schemas/vote.ts
+++ libs/duality-social-lib/src/lib/schemas/vote.ts
@@ -1,9 +1,10 @@
 import { Schema } from "mongoose";
 import { IVote } from "../interfaces/vote";
 import ModelName from "../enumerations/modelName";
+import { HumanityTypeEnum } from "../enumerations/humanityType";
 
 export const VoteSchema = new Schema<IVote>({
     postId: { type: Schema.Types.ObjectId, ref: ModelName.Post, required: true, immutable: true },
-    humanity: { type: String, required: true, immutable: true },
+    humanity: { type: String, required: true, enum: Object.values(HumanityTypeEnum), immutable: true },
     createdBy: { type: Schema.Types.ObjectId, ref: ModelName.User, required: true, immutable: true },
 }, { timestamps: true });
