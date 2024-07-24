a00475e72e31a2ee34f5a55876f6454eeb06a0e9
Jessica Mulein <jessica@digitaldefiance.org>
2024-07-24 18:08:25 +0000

    finish ripping out auth0

--- apps/duality-social-react/src/components/LoginLink.tsx
+++ apps/duality-social-react/src/components/LoginLink.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
 import './LoginLink.scss';
 
 function LoginLink() {
-  const { loginWithRedirect } = useAuth0();
 
   return (
     <button 
-      onClick={() => loginWithRedirect()} 
       className="button-as-link"
     >
       Log In
